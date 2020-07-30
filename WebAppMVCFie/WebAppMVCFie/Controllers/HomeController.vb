076a74928eab422a4d23f00e7ed96cb3bcb40678
MarcoErlwein <33732800+MarcoErlwein@users.noreply.github.com>
2020-07-30 17:50:45 -0400

    Update HomeController.vb

--- WebAppMVCFie/WebAppMVCFie/Controllers/HomeController.vb
+++ WebAppMVCFie/WebAppMVCFie/Controllers/HomeController.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.Mvc.Controller
 
     Function Index() As ActionResult
-        '' Cambio en GitHub
+        '' Cambio por CI desde Github
         Return View()
     End Function
 
